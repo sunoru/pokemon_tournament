e3ea1302a83d910acc677097e5b45e3e6c7aec01
sunoru <sunoru@live.com>
2014-09-27 23:42:18 +0800

    todo url

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', 'pmtour.views.home', name='home'),
+                       url(r'^admin/', include(admin.site.urls)),
 )
+
