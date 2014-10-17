b674cc913790e783a7c6746236deb966e4696a80
sunoru <s@sunoru.com>
2014-10-17 16:25:12 +0800

    make init

--- pmtour/tests.py
+++ pmtour/tests.py
@@ -7,13 +7,24 @@ import random
 def add_test_users(n, m=0):
     for i in xrange(m, n):
         pwd = "%s" % random.randint(100000, 999999)
-        usr = "test%s" % (i + 1)
+        usr = "%s_test%s" % (datetime.date.today().strftime("%y%m%d"), i + 1)
         user = User.objects.create_user(usr, "%s@moon.moe" % usr, pwd)
         playeruser = PlayerUser.objects.create(user=user, name=user.username, player_id=user.username)
-        print usr, pwd
+        print "add new user: %s %s" % (usr, pwd)
 
 
 def init():
     Option.objects.create(option_name="uid", option_value="-1")
     playeruser = PlayerUser.objects.create(user=User.objects.all()[0], name="admin", player_id="root")
-    print playeruser
+    import getpass
+    usr = raw_input("Username: ")
+    email = raw_input("Email(default: %s@moon.moe): " % usr)
+    if email == "":
+        email = "%s@moon.moe" % usr
+    pas = getpass.getpass()
+    newuser = User.objects.create_superuser(usr, email, pas)
+    print "add new superuser: %s" % newuser
+    num = input("the number of test players: ")
+    add_test_users(num)
+
+
