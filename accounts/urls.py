ea28868cd34b44b6247fbb3e2b3e200cc98b196b
sunoru sidw <s@sunoru.com>
2016-04-30 16:01:52 +0800

    urls..

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import patterns, include, url
+import accounts.views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'accounts.views.home', name='home'),
+    url(r'^$', accounts.views.home, name='home'),
 
-    url(r'^tournaments/$', 'accounts.views.tournaments', name='tournaments'),
-    url(r'^playerusers/$', 'accounts.views.playerusers', name='playerusers'),
-    url(r'^login/$', 'accounts.views.login_view', name='login'),
-    url(r'^logout/$', 'accounts.views.logout_view', name='logout'),
-    url(r'^create/$', 'accounts.views.create', name='create'),
-    url(r'^load/$', 'accounts.views.load', name='load'),
-    url(r'^edit/$', 'accounts.views.edit', name='edit'),
-    url(r'^edit/(?P<player_id>.+?)/$', 'accounts.views.player_setting', name='player_setting'),
-    url(r'^(?P<player_id>.+?)/$', 'accounts.views.player_view', name='player_view'),
-    url(r'^(?P<player_id>.+?)/logs$', 'accounts.views.player_logs', name='player_view'),
+    url(r'^tournaments/$', accounts.views.tournaments, name='tournaments'),
+    url(r'^playerusers/$', accounts.views.playerusers, name='playerusers'),
+    url(r'^login/$', accounts.views.login_view, name='login'),
+    url(r'^logout/$', accounts.views.logout_view, name='logout'),
+    url(r'^create/$', accounts.views.create, name='create'),
+    url(r'^load/$', accounts.views.load, name='load'),
+    url(r'^edit/$', accounts.views.edit, name='edit'),
+    url(r'^edit/(?P<player_id>.+?)/$', accounts.views.player_setting, name='player_setting'),
+    url(r'^(?P<player_id>.+?)/$', accounts.views.player_view, name='player_view'),
+    url(r'^(?P<player_id>.+?)/logs$', accounts.views.player_logs, name='player_view'),
 )
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import django.shortcuts
+import mysite.views
 
 #from pmtour import views
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'mysite.views.index', name='index'),
-    url(r'^favicon\.ico$', 'django.shortcuts.redirect', {'to': '/static/images/favicon.ico'}),
+    url(r'^$', mysite.views.index, name='index'),
+    url(r'^favicon\.ico$', django.shortcuts.redirect, {'to': '/static/images/favicon.ico'}),
     url(r'^django_admin/', include(admin.site.urls)),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^(?P<tour_id>.+?)/', include('pmtour.urls', namespace='tournament')),
