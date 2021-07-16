f38834a73cf0d84b19de3be8bbf0ff5903b5640f
Sunoru <s@sunoru.com>
2021-07-15 20:18:42 -0400

    Use correct template path.

--- mysite/settings_default.py
+++ mysite/settings_default.py
@@ -55,7 +55,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'templates'
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
